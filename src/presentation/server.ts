e3e424a3e86b9a5d3420bfb8ac2baadf54e3bba6
CristianDaniel1 <cristiandaniel.1050@gmail.com>
2024-12-08 17:27:20 -0300

    Criação de novo caso de uso de múltiplos serviços

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,4 @@
-import { CheckService } from '../domain/use-cases/checks/check-service';
-import { SendEmailLogs } from '../domain/use-cases/email/send-email-logs';
+import { CheckServiceMultiple } from '../domain/use-cases/checks/check-service-multiple';
 import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasource';
 import { MongoLogDatasource } from '../infrastructure/datasources/mongo-log.datasource';
 import { PostgresLogDatasource } from '../infrastructure/datasources/postgres-log.datasource';
@@ -7,11 +6,14 @@ import { LogRepositoryImpl } from '../infrastructure/repositories/log-repository
 import { CronService } from './cron/cron-service';
 import { EmailService } from './email/email-service';
 
-const logRepository = new LogRepositoryImpl(
-  // new FileSystemDatasource()
-  // new MongoLogDatasource()
+const fsLogRepository = new LogRepositoryImpl(new FileSystemDatasource());
+
+const mongoLogRepository = new LogRepositoryImpl(new MongoLogDatasource());
+
+const postgresLogRepository = new LogRepositoryImpl(
   new PostgresLogDatasource()
 );
+
 const emailService = new EmailService();
 
 export class Server {
@@ -31,14 +33,14 @@ export class Server {
     //   'noc.node2@gmail.com',
     // ]);
 
-    CronService.createJob('*/5 * * * * *', () => {
-      const url = 'https://google.com';
+    // CronService.createJob('*/5 * * * * *', () => {
+    //   const url = 'https://google.com';
 
-      new CheckService(
-        logRepository,
-        () => console.log(`${url} is ok`),
-        error => console.log(error)
-      ).execute(url);
-    });
+    //   new CheckServiceMultiple(
+    //     [fsLogRepository, postgresLogRepository, mongoLogRepository],
+    //     () => console.log(`${url} is ok`),
+    //     error => console.log(error)
+    //   ).execute(url);
+    // });
   }
 }
